7678ca84e1016d7ef67f9b3da7f9e20b6d454a98
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-10-31 11:49:29 +0100

    update firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,16 @@
 // Для роботи із firebase обовʼязково треба ініціалізувати проект
 import { initializeApp } from "firebase/app";
 // Функція для підключення авторизації в проект
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+// // Функція для підключення авторизації в проект
+// import { getAuth } from "firebase/auth";
 // Функція для підключення бази даних у проект
 import { getFirestore } from "firebase/firestore";
 // Функція для підключення сховища файлів в проект
 import { getStorage } from "firebase/storage";
 // Функція для підключення аналітики в проект
 import { getAnalytics } from "firebase/analytics";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // TODO: add ReactNativePersistance from tutor github
 // Your web app's Firebase configuration
@@ -26,7 +29,12 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// Ініціалізація Auth з AsyncStorage для роботи редакс персистора
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const analytics = getAnalytics(app);
--- redux/auth/authOperations.js
+++ redux/auth/authOperations.js
@@ -6,7 +6,7 @@ import {
   onAuthStateChanged,
   updateProfile,
 } from "firebase/auth";
-import { auth } from "../../firebase/config";
+import { auth } from "../../firebaseConfig";
 
 export const registerDB = createAsyncThunk(
   "auth/signup",
